4cbf571d83bda31a2c265390a1d7426fa89452a4
Orjan De Smet <orjan.de.smet@gmail.com>
2016-04-27 18:09:29 +0200

    Initial Commit

---
+++ ODSNetworking/ODSNetworking.h
@@ -0,0 +1,19 @@
+//
+//  ODSNetworking.h
+//  ODSNetworking
+//
+//  Created by Orjan De Smet on 27/04/16.
+//  Copyright © 2016 Orjan De Smet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ODSNetworking.
+FOUNDATION_EXPORT double ODSNetworkingVersionNumber;
+
+//! Project version string for ODSNetworking.
+FOUNDATION_EXPORT const unsigned char ODSNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ODSNetworking/PublicHeader.h>
+
+
